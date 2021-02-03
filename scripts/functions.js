135f16a22147574a067aa9e2133882b3badcb2f9
Mathieu Gaudreau <matgaudreau13@gmail.com>
2021-02-03 16:52:51 -0500

    lightbox

--- scripts/functions.js
+++ scripts/functions.js
@@ -8,13 +8,23 @@ function openlb() {
     }
 
     function closelb(){
-        document.getElementById("iViewer").style.display = "none";
-        document.getElementById("iViewerimg").style.display = "none";
+        
+        document.getElementById("iViewer").style.animationName = "fondFerme";
+        document.getElementById("iViewerimg").style.animationName = "lightboxFerme";
+
+        setTimeout(function() {
+            document.getElementById("iViewer").style.display = "none";
+            document.getElementById("iViewerimg").style.display = "none";
+              }, 300);
+
+       
     }
 
     function prendImg(element){
         var imgSrc = element.src;
 
+        document.getElementById("iViewer").style.animationName = "fondOuvre";
+        document.getElementById("iViewerimg").style.animationName = "lightboxOuvre";
         document.getElementById("iViewer").style.display = "flex";
         document.getElementById("iViewerimg").style.display = "block";
         document.getElementById("iViewerimg").setAttribute("src", imgSrc);
--- scripts/initialize.js
+++ scripts/initialize.js
@@ -6,7 +6,6 @@
   */
  (function() {
 
-
   //categories
   document.getElementById("web").addEventListener('mouseup',function () {
     document.getElementById("primary").style.display = "none";
@@ -35,213 +34,182 @@
     document.getElementById("ecranJeux").style.animation="popupOpen 0.2s"
   });
 
-  let boutons = document.getElementsByClassName('boutonCat');
-  let ecranCategories = document.getElementsByClassName('ecranCategorie');
-
-  for (let i = 0; i < boutons.length; i++) {
-    boutons[i].onclick = fermeCat;
-  }
-
-  function fermeCat() {
-    for (let i = 0; i < ecranCategories.length; i++) 
-    {
-      ecranCategories[i].style.animation="popupClose 0.3s"
-      setTimeout(function() {
-      ecranCategories[i].style.display = "none";
-        }, 300);
-    }
-
-    document.getElementById("primary").style.display = "flex";
-    document.getElementById("footer").style.display = "flex";
-
-  }  
-  
 
   //projets
   //last spark
   document.getElementById('lastSpark').addEventListener('mouseup',function () {
     document.getElementById("popupLastSpark").style.display = "flex";
-    document.getElementById('listeProjetJeux').style.display = "none";
     document.getElementById('listePopJeux').style.display = "flex";
+    document.getElementById("popupLastSpark").style.animation="popupOpen 0.2s"
   });
-
-  document.getElementById('boutonLS').addEventListener('mouseup',function () {
-    document.getElementById('popupLastSpark').style.display = "none";
-    document.getElementById("listeProjetJeux").style.display = "flex";
-  });
-
 
 
   //tim 2020
   document.getElementById('TIM2020').addEventListener('mouseup',function () {
     document.getElementById("popupTIM2020").style.display = "flex";
-    document.getElementById('listeProjetWeb').style.display = "none";
     document.getElementById('listePopWeb').style.display = "flex";
-  });
-
-  document.getElementById('boutonTIM').addEventListener('mouseup',function () {
-    document.getElementById('popupTIM2020').style.display = "none";
-    document.getElementById("listeProjetWeb").style.display = "flex";
+    document.getElementById("popupTIM2020").style.animation="popupOpen 0.2s"
   });
 
 
   //vieux four
   document.getElementById('VFM').addEventListener('mouseup',function () {
     document.getElementById("popupVFM").style.display = "flex";
-    document.getElementById('listeProjetWeb').style.display = "none";
     document.getElementById('listePopWeb').style.display = "flex";
+    document.getElementById("popupVFM").style.animation="popupOpen 0.2s"
   });
-
-  document.getElementById('boutonVFM').addEventListener('mouseup',function () {
-    document.getElementById('popupVFM').style.display = "none";
-    document.getElementById("listeProjetWeb").style.display = "flex";
-  });
-
-
 
   //site last spark
   document.getElementById('SLS').addEventListener('mouseup',function () {
     document.getElementById("popupSLS").style.display = "flex";
-    document.getElementById('listeProjetWeb').style.display = "none";
     document.getElementById('listePopWeb').style.display = "flex";
+    document.getElementById("popupSLS").style.animation="popupOpen 0.2s"
   });
   
-  document.getElementById('boutonSLS').addEventListener('mouseup',function () {
-    document.getElementById('popupSLS').style.display = "none";
-    document.getElementById("listeProjetWeb").style.display = "flex";
-  });
-
 
   //NIMH
   document.getElementById('NIMH').addEventListener('mouseup',function () {
     document.getElementById("popupNIMH").style.display = "flex";
-    document.getElementById('listeProjetJeux').style.display = "none";
     document.getElementById('listePopJeux').style.display = "flex";
+    document.getElementById("popupNIMH").style.animation="popupOpen 0.2s"
   });
 
-  document.getElementById('boutonNIMH').addEventListener('mouseup',function () {
-    document.getElementById('popupNIMH').style.display = "none";
-    document.getElementById("listeProjetJeux").style.display = "flex";
-  });
 
   //ASSASINE
-    document.getElementById('ASSASINE').addEventListener('mouseup',function () {
-      document.getElementById("popupASSASINE").style.display = "flex";
-      document.getElementById('listeProjetJeux').style.display = "none";
-      document.getElementById('listePopJeux').style.display = "flex";
-    });
+  document.getElementById('ASSASINE').addEventListener('mouseup',function () {
+    document.getElementById("popupASSASINE").style.display = "flex";
+    document.getElementById('listePopJeux').style.display = "flex";
+    document.getElementById("popupASSASINE").style.animation="popupOpen 0.2s"
+  });
   
-    document.getElementById('boutonASSASINE').addEventListener('mouseup',function () {
-      document.getElementById('popupASSASINE').style.display = "none";
-      document.getElementById("listeProjetJeux").style.display = "flex";
-    });
 
    //Veille annonce
-   document.getElementById('MCDO').addEventListener('mouseup',function () {
+  document.getElementById('MCDO').addEventListener('mouseup',function () {
     document.getElementById("popupMCDO").style.display = "flex";
-    document.getElementById('listeProjetPhoto').style.display = "none";
     document.getElementById('listePopPhoto').style.display = "flex";
+    document.getElementById("popupMCDO").style.animation="popupOpen 0.2s"
   });
 
-  document.getElementById('boutonMCDO').addEventListener('mouseup',function () {
-    document.getElementById('popupMCDO').style.display = "none";
-    document.getElementById("listeProjetPhoto").style.display = "flex";
+
+  //TUTO
+  document.getElementById('TUTO').addEventListener('mouseup',function () {
+    document.getElementById("popupTUTO").style.display = "flex";
+     document.getElementById('listePopPhoto').style.display = "flex";
+    document.getElementById("popupTUTO").style.animation="popupOpen 0.2s"
   });
-
-     //TUTO
-     document.getElementById('TUTO').addEventListener('mouseup',function () {
-      document.getElementById("popupTUTO").style.display = "flex";
-      document.getElementById('listeProjetPhoto').style.display = "none";
-      document.getElementById('listePopPhoto').style.display = "flex";
-    });
   
-    document.getElementById('boutonTUTO').addEventListener('mouseup',function () {
-      document.getElementById('popupTUTO').style.display = "none";
-      document.getElementById("listeProjetPhoto").style.display = "flex";
-    });
 
-     //Objets
-     document.getElementById('OBJETS').addEventListener('mouseup',function () {
-      document.getElementById("popupOBJETS").style.display = "flex";
-      document.getElementById('listeProjetPhoto').style.display = "none";
-      document.getElementById('listePopPhoto').style.display = "flex";
-    });
+  //Objets
+  document.getElementById('OBJETS').addEventListener('mouseup',function () {
+    document.getElementById("popupOBJETS").style.display = "flex";
+    document.getElementById('listePopPhoto').style.display = "flex";
+    document.getElementById("popupOBJETS").style.animation="popupOpen 0.2s"
+  });
   
-    document.getElementById('boutonOBJETS').addEventListener('mouseup',function () {
-      document.getElementById('popupOBJETS').style.display = "none";
-      document.getElementById("listeProjetPhoto").style.display = "flex";
-    });
+    // document.getElementById('boutonOBJETS').addEventListener('mouseup',function () {
+    //   document.getElementById('popupOBJETS').style.display = "none";
+    //   document.getElementById("listeProjetPhoto").style.display = "flex";
+    // });
 
-         //Rovesciato
-         document.getElementById('ROVE').addEventListener('mouseup',function () {
-          document.getElementById("popupROVE").style.display = "flex";
-          document.getElementById('listeProjetPhoto').style.display = "none";
-          document.getElementById('listePopPhoto').style.display = "flex";
-        });
+  //Rovesciato
+  document.getElementById('ROVE').addEventListener('mouseup',function () {
+    document.getElementById("popupROVE").style.display = "flex";
+    document.getElementById('listePopPhoto').style.display = "flex";
+    document.getElementById("popupROVE").style.animation="popupOpen 0.2s"
+  });
       
-        document.getElementById('boutonROVE').addEventListener('mouseup',function () {
-          document.getElementById('popupROVE').style.display = "none";
-          document.getElementById("listeProjetPhoto").style.display = "flex";
-        });
 
-    
-           //Bloodville
-           document.getElementById('BLOOD').addEventListener('mouseup',function () {
-            document.getElementById("popupBLOOD").style.display = "flex";
-            document.getElementById('listeProjetJeux').style.display = "none";
-            document.getElementById('listePopJeux').style.display = "flex";
-          });
+  //Bloodville
+  document.getElementById('BLOOD').addEventListener('mouseup',function () {
+    document.getElementById("popupBLOOD").style.display = "flex";
+    document.getElementById('listePopJeux').style.display = "flex";
+    document.getElementById("popupBLOOD").style.animation="popupOpen 0.2s"
+  });
         
-          document.getElementById('boutonBLOOD').addEventListener('mouseup',function () {
-            document.getElementById('popupBLOOD').style.display = "none";
-            document.getElementById("listeProjetJeux").style.display = "flex";
-          }); 
-          
-               //Manor
-               document.getElementById('MANOR').addEventListener('mouseup',function () {
-                document.getElementById("popupMANOR").style.display = "flex";
-                document.getElementById('listeProjetJeux').style.display = "none";
-                document.getElementById('listePopJeux').style.display = "flex";
-              });
+
+  //Manor
+  document.getElementById('MANOR').addEventListener('mouseup',function () {
+    document.getElementById("popupMANOR").style.display = "flex";
+    document.getElementById('listePopJeux').style.display = "flex";
+    document.getElementById("popupMANOR").style.animation="popupOpen 0.2s"
+  });
             
-              document.getElementById('boutonMANOR').addEventListener('mouseup',function () {
-                document.getElementById('popupMANOR').style.display = "none";
-                document.getElementById("listeProjetJeux").style.display = "flex";
-              });      
-    
-         //Obelisque
-         document.getElementById('OBE').addEventListener('mouseup',function () {
-          document.getElementById("popupOBE").style.display = "flex";
-          document.getElementById('listeProjetPhoto').style.display = "none";
-          document.getElementById('listePopPhoto').style.display = "flex";
-        });
+
+  //Obelisque
+    document.getElementById('OBE').addEventListener('mouseup',function () {
+      document.getElementById("popupOBE").style.display = "flex";
+      document.getElementById('listePopPhoto').style.display = "flex";
+      document.getElementById("popupOBE").style.animation="popupOpen 0.2s"
+  });
       
-        document.getElementById('boutonOBE').addEventListener('mouseup',function () {
-          document.getElementById('popupOBE').style.display = "none";
-          document.getElementById("listeProjetPhoto").style.display = "flex";
-        });
 
-                 //Sous-Marin
-                 document.getElementById('SM').addEventListener('mouseup',function () {
-                  document.getElementById("popupSM").style.display = "flex";
-                  document.getElementById('listeProjetPhoto').style.display = "none";
-                  document.getElementById('listePopPhoto').style.display = "flex";
-                });
+  //Sous-Marin
+  document.getElementById('SM').addEventListener('mouseup',function () {
+    document.getElementById("popupSM").style.display = "flex";
+    document.getElementById('listePopPhoto').style.display = "flex";
+    document.getElementById("popupSM").style.animation="popupOpen 0.2s"
+  });
               
-                document.getElementById('boutonSM').addEventListener('mouseup',function () {
-                  document.getElementById('popupSM').style.display = "none";
-                  document.getElementById("listeProjetPhoto").style.display = "flex";
-                });
+  document.getElementById("iViewer").addEventListener('mouseup', closelb);
 
-    
-                //document.querySelectorAll(".slides img").addEventListener('mouseup', openlb);
-                document.getElementById("iViewer").addEventListener('mouseup', closelb);
+  const images = document.querySelectorAll(".slides img");
 
-                const images = document.querySelectorAll(".slides img");
+  for (let i = 0; i < images.length; i++) 
+  {
+    images[i].addEventListener("mouseup", function() 
+    {
+      prendImg(this);
+    });
+  }
 
-                for (let i = 0; i < images.length; i++) {
-                    images[i].addEventListener("mouseup", function() {
-                      prendImg(this);
-                    });
-                }
-  })();
+
+  const boutonsPop = document.querySelectorAll(".boutonPop");
+
+  for (let i = 0; i < boutonsPop.length; i++) 
+  {
+    boutonsPop[i].addEventListener("mouseup",function () 
+    {
+      fermePop();
+    });       
+  }
+
+const popup = document.querySelectorAll(".popup");
+
+function fermePop() 
+{
+  for (let i = 0; i < popup.length; i++) 
+  {
+    popup[i].style.animation="popupClose 0.3s"
+    setTimeout(function() 
+    {
+      popup[i].style.display = "none";
+    }, 300);
+  }              
+}  
+
+
+let boutonsCat = document.getElementsByClassName('boutonCat');
+let ecranCategories = document.getElementsByClassName('ecranCategorie');
+
+for (let i = 0; i < boutonsCat.length; i++) {
+  boutonsCat[i].addEventListener("mouseup",function () 
+  {
+    fermeCat();
+  });       
+}
+
+function fermeCat() 
+{
+  for (let i = 0; i < ecranCategories.length; i++) 
+  {
+    ecranCategories[i].style.animation="popupClose 0.3s"
+    setTimeout(function() {
+    ecranCategories[i].style.display = "none";
+    }, 300);
+  }
+
+  document.getElementById("primary").style.display = "flex";
+  document.getElementById("footer").style.display = "flex";
+}  
+  
+})();
